54ee2b9335e3f52c0f22553076fb2d30147edf64
ChaoticPan <elisabeth.kintzel@student.htw-berlin.de>
2023-06-17 09:49:55 +0200

    split input image into smaller section and save them as new png

--- Assets/Scripts/ImgReader.cs
+++ Assets/Scripts/ImgReader.cs
@@ -1,41 +1,68 @@
-using System.Collections;
-using System.Collections.Generic;
 using System.IO;
-using System.Runtime.Serialization.Formatters.Binary;
-
 using UnityEditor;
 using UnityEngine;
-using UnityEngine.WSA;
-using Application = UnityEngine.Application;
 
 public class ImgReader : MonoBehaviour
 {
     [SerializeField] Sprite inputImage;
-    public Vector2 ImgSize;
-    public int GidSize = 1;
+    Vector2 imgSize;
+    public int gridSize;
+    public int gridOffset;
     public string saveLocation = "Assets/tilesetNEW";
+    int tileNumber = 1;
 
     // Start is called before the first frame update
     void Start()
     {
-        ImgSize = inputImage.rect.size;
-        
-        Texture2D tex = inputImage.texture;
-        // Texture2D neTEx = new Texture2D(32,32);
-        // neTEx=tex.GetRawTextureData()
-        byte[] bytes = tex.EncodeToPNG();
+        imgSize = inputImage.rect.size;
 
-        Debug.Log(tex.height+ "x" +tex.width);
-        Debug.Log(bytes.Length);
-        saveTile(bytes, "00");
-        // path = saveLocation + "/Path 00";
-        // FileUtil.CopyFileOrDirectory(path, saveLocation);
+        Color[] pixels = inputImage.texture.GetPixels();
+        Texture2D tex = new Texture2D(gridSize, gridSize);
+        Color[] newPixels = new Color[gridSize*gridSize];
+        int p = 0;
+        byte[] bytes;
+        
+        newPixels = new Color[gridSize*gridSize];
+        p = 0;
+        // for (int i = 0; i < imgSize.x; i+=gridOffset)
+        // {
+        //     for (int q = i; q < i+gridSize; q++)
+        //     {
+        //         Debug.Log(q);
+        //
+        //         // int Ix = (x < imgSize.x) ? x : x - (int)imgSize.x;
+        //         // int Iy = (y < imgSize.y) ? y : y - (int)imgSize.y;
+        //         // int pos = (int)(Ix + Iy * imgSize.x);
+        //     }
+        //     Debug.Log("NEW LOOP");
+        //     Debug.Log(i);
+        // }
+        p = 0;
+        for (int y = 0; y < gridSize; y++)
+        {
+            for (int x = (int)(1.5*gridSize); x < (int)(2.5*gridSize); x++)
+            {
+                int Iy = (y < imgSize.y) ? y : y - (int)imgSize.y;
+                int Ix = (x < imgSize.x) ? x : x - (int)imgSize.x;
+
+                int pos = (int)(Ix + Iy * imgSize.x);
+                newPixels[p] = pixels[pos];
+                p++;
+            }
+        }
+        tex.SetPixels(newPixels, 0);
+        bytes = tex.EncodeToPNG();
+        saveTile(bytes, tileNumber);
     }
 
-    void saveTile(byte[] tilebytes, string fileName)
+    void saveTile(byte[] tilebytes, int fileNumber)
     {
+        string fileName = (fileNumber < 10) ? "0" + fileNumber : fileNumber.ToString();
+        tileNumber++;
         string path = saveLocation + "/" + fileName + ".png";
         File.WriteAllBytes(path, tilebytes);
         AssetDatabase.ImportAsset(path);
     }
+    
+    
 }
